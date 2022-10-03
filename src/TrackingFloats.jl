bb70c56632f57cbf61b697e76a444fb0d733ce06
JonasH <hanssonjonas96@gmail.com>
2022-10-03 22:46:21 +0200

    Added + and - operations

--- src/TrackingFloats.jl
+++ src/TrackingFloats.jl
@@ -14,5 +14,23 @@ struct TrackingFloat <: AbstractFloat
     x_max::Float64
 end
 
+"""
+Constructs a TrackingFloat
+# Usage:
+    tf=TrackingFloat(x)
+"""
+TrackingFloat(x)=TrackingFloat(copy(x),zero(x))
+
+# Add and subtraction of 2 TrackingFloats.
+"""
+# Usage:
+x_3=+(x_1::TrackingFloat,x_2::TrackingFloat)=x_1+x_2
+"""
++(x_1::TrackingFloat,x_2::TrackingFloat) = TrackingFloat(x_1.x+x_2.x,maximum(abs.([x_1.x,x_1.x_max,x_2.x,x_2.x_max])))
+"""
+# Usage:
+-(x_1::TrackingFloat,x_2::TrackingFloat)=x_1-x_2
+"""
+-(x_1::TrackingFloat,x_2::TrackingFloat) = TrackingFloat(x_1.x-x_2.x,maximum(abs.([x_1.x,x_1.x_max,x_2.x,x_2.x_max])))
 
 end
