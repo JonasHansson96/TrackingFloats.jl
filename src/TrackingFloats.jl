f1e65f5f2d67854ff3f3ddce2ece971a7f7fbafa
JonasH <hanssonjonas96@gmail.com>
2022-10-03 22:26:24 +0200

    Added object and imports

--- src/TrackingFloats.jl
+++ src/TrackingFloats.jl
@@ -1,5 +1,12 @@
 module TrackingFloats
 
-# Write your package code here.
+using LinearAlgebra
+import Base: +,-,*,/, promote_rule,<,sqrt
+
+struct TrackingFloat <: AbstractFloat
+    x::Float64
+    x_max::Float64
+end
+
 
 end
