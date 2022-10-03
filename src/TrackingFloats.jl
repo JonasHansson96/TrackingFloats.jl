08e9e154dffaee8933adcd0c406c7a3a51ae13c9
JonasH <hanssonjonas96@gmail.com>
2022-10-03 22:30:13 +0200

    Added docstring to TrackingFloats

--- src/TrackingFloats.jl
+++ src/TrackingFloats.jl
@@ -3,6 +3,12 @@ module TrackingFloats
 using LinearAlgebra
 import Base: +,-,*,/, promote_rule,<,sqrt
 
+"""
+TrackingFloat
+# Fields:
+    x::Float64   current value
+    x_max::Float64  maximum value in creation.
+"""
 struct TrackingFloat <: AbstractFloat
     x::Float64
     x_max::Float64
