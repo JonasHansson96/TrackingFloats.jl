29423749aa24de3c2f20f21b8f927590b7cf9d9f
JonasH <hanssonjonas96@gmail.com>
2022-10-03 22:58:31 +0200

    Added * / functions and value and getmax

--- src/TrackingFloats.jl
+++ src/TrackingFloats.jl
@@ -26,11 +26,29 @@ TrackingFloat(x)=TrackingFloat(copy(x),zero(x))
 # Usage:
 x_3=+(x_1::TrackingFloat,x_2::TrackingFloat)=x_1+x_2
 """
-+(x_1::TrackingFloat,x_2::TrackingFloat) = TrackingFloat(x_1.x+x_2.x,maximum(abs.([x_1.x,x_1.x_max,x_2.x,x_2.x_max])))
++(x_1::TrackingFloat,x_2::TrackingFloat) = TrackingFloat(x_1.x+x_2.x,max(abs(x_1.x),abs(x_1.x_max),abs(x_2.x),abs(x_2.x_max)))
 """
 # Usage:
 -(x_1::TrackingFloat,x_2::TrackingFloat)=x_1-x_2
 """
--(x_1::TrackingFloat,x_2::TrackingFloat) = TrackingFloat(x_1.x-x_2.x,maximum(abs.([x_1.x,x_1.x_max,x_2.x,x_2.x_max])))
+-(x_1::TrackingFloat,x_2::TrackingFloat) = TrackingFloat(x_1.x-x_2.x,max(abs(x_1.x),abs(x_1.x_max),abs(x_2.x),abs(x_2.x_max)))
+
+# Multiplication and division
+"""
+# Usage:
+*(x_1::TrackingFloat,x_2::TrackingFloat)=x_1*x_2
+"""
+*(x_1::TrackingFloat,x_2::TrackingFloat) = TrackingFloat(x_1.x*x_2.x,max(abs(x_1.x),abs(x_1.x_max),abs(x_2.x),abs(x_2.x_max)))
+"""
+# Usage:
+/(x_1::TrackingFloat,x_2::TrackingFloat)=x_1/x_2
+"""
+function /(x_1::TrackingFloat,x_2::TrackingFloat) 
+    den=1/x_2.x
+    TrackingFloat(x_1.x*den,max(abs(x_1.x),abs(x_1.x_max),abs(den),abs(x_2.x_max)))
+end
+
+value(tf::TrackingFloat)=tf.x
+getmax(tf::TrackingFloat)=tf.x_max
 
 end
