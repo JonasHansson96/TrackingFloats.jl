9d34f4f774b77aad7f7150fd6ba28a3130b55d0d
JonasH <hanssonjonas96@gmail.com>
2022-10-03 23:29:49 +0200

    Added some basic tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,10 @@ using Test
 
 @testset "TrackingFloats.jl" begin
     # Write your tests here.
+    using Test
+    v = TrackingFloat(1.0) + TrackingFloat(3.0) # We expect TrackingFloat(4, 3)
+    @test v     == TrackingFloat(4,3)           # which we test using the macro @test
+    @test v*v   == TrackingFloat(16, 4)
+    @test v - v == TrackingFloat(0, 4)
+    @test v/TrackingFloat(0.1, 0) == TrackingFloat(40, 10)
 end
